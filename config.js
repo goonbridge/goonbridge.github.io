401ba6723708711113147816ba6671d3e064e936
Todd Weir <todd.weir@gmail.com>
2023-12-02 22:15:55 +1300

    Configured loc, weather

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const CONFIG = {
 	// General
 	name: 'John',
 	imageBackground: true,
-	openInNewTab: true,
+	openInNewTab: false,
 	twelveHourFormat: false,
 
 	// Greetings
@@ -26,14 +26,14 @@ const CONFIG = {
 	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
 
 	// Weather
-	weatherKey: 'InsertYourAPIKeyHere123456', // Write here your API Key
+	weatherKey: '06fa621c43eee6998b744f8f9eec481f', // Write here your API Key
 	weatherIcons: 'Nord', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
 	trackLocation: false, // If false or an error occurs, the app will use the lat/lon below
-	defaultLatitude: '37.775',
-	defaultLongitude: '-122.419',
+	defaultLatitude: '-43.487',
+	defaultLongitude: '-172.536',
 
 	// Autochange
 	autoChangeTheme: true,
